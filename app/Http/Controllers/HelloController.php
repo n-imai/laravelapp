f43a5f8f4ff72907ed303e45f9ba0660094b8f2a
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-08-10 10:24:09 +0900

    Adding parameters to the controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,19 +6,23 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(){
+    public function index($id='noname',$pass='unknown'){
         return <<<EOF
         <HTML>
         <HEAD>
         <title>Hello/Index</title>
         <style>
-        body {font-size:100px;text-align:right; color:#eee;
+        body {font-size:16pt; color:#999;}
+        h1 {font-size:100px;text-align:right; color:#eee;
         margin: -40px 0px -50px 0px; }
         </style>
         </head>
         <body>
             <h1>Index</h1>
             <p>これは、Helloコントローラーのindexアクションです。</p>
+            <ul>
+                <li>ID:{$id}</li>
+                <li>PASS:{$pass}</li>
         </body>
         </html>
         EOF;
